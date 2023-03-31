b52e6924f351de9e47d3020aae370b73b22e9a61
Ragavendra Vigneshwaran R <ragav@Ragav.localdomain>
2023-03-31 18:25:30 +0530

    Added github actions

--- automation-scripts/testin.sh
+++ automation-scripts/testin.sh
@@ -1,2 +1,65 @@
-#!/bin/bash
-echo $SHARED_EC2_NAME@$SHARED_EC2_IP
+#! /bin/bash
+###############################################################################
+# Name: getkeda.sh
+###############################################################################
+# Description:
+# To deploy keda for the required tenants
+###############################################################################
+# Function Declaration
+###############################################################################
+function exit_error {
+  echo "[`date`] ${*}"
+  exit 1
+}
+
+# Write message to stdout
+function console_msg {
+  echo "[`date`] ${*}"
+}
+
+# Usage of this script
+usage() {
+  echo " --sharedIP   -  EC2 instance login IP"
+  echo " --sharedName -  EC2 login name"
+  echo " -h|--help    - Show this help message"
+  echo ""
+  exit 1
+}
+
+parseArgs(){
+  while test $# -ge 1; do
+    arg=$1
+    shift
+    case $arg in
+	  --sharedIP)
+        sharedIP=${1}
+      ;;
+	  --sharedName)
+        sharedName=${1}
+        shift
+      ;;
+	  -h|--help)
+        usage
+      ;;
+	esac
+  done
+  if ! [[ $sharedIP && $sharedName  ]]
+  then
+    console_msg "Error: Invalid Arguments"
+    usage
+  fi
+}
+
+#Get information on Keda
+function print_ip()
+{
+    echo ${1}@${2}
+}
+
+
+###############################################################################
+# Main Declaration
+###############################################################################
+# Parse input arguments
+parseArgs "$@"
+print_ip $sharedName $sharedIP

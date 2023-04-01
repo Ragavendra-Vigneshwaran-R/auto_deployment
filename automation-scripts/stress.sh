b8cb7ff0292192a326f59c621da7b83691479f72
Ragavendra Vigneshwaran R <ragav@Ragav.localdomain>
2023-04-01 16:18:50 +0530

    keda disabled

--- automation-scripts/stress.sh
+++ automation-scripts/stress.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl exec -it nodejs-application-rvig-dep-589d694498-rxtlh -n ragav -- /bin/bash -c "while true;do curl localhost;done;"
+kubectl exec -it nodejs-application-rvig-dep-749c5b9c7f-6d2nd -n ragav -- /bin/bash -c "while true;do curl localhost;done;"
 

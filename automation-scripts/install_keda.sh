87dc6d29b8b57b6d2aba12fb573b328354724c74
rvig <rvig@softwareag.com>
2023-04-03 14:12:21 +0530

    Testing self hosted runner

--- automation-scripts/install_keda.sh
+++ automation-scripts/install_keda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 helm repo add kedacore https://kedacore.github.io/charts
 helm repo update
-kubectl create namespace keda
-helm install keda kedacore/keda --namespace keda
+kubectl create namespace keda-server
+helm install keda kedacore/keda --namespace keda-server
